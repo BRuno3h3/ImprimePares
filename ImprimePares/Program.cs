98272babd4af92dd82890149126b6b143ff72577
BRuno3h3 <bruno.tomasi16@gmail.com>
2024-01-24 21:20:34 -0300

    Adicionar arquivos de projeto.

---
+++ ImprimePares/Program.cs
@@ -0,0 +1,24 @@
+﻿List<int> numeroPar = new List<int>();
+
+
+int i = 0;
+
+while(i <= 100) 
+    {
+        if(i % 2 == 0) 
+        {     
+            numeroPar.Add(i);
+        }
+        i++;
+    };
+
+foreach(int num in numeroPar)
+{
+    Console.WriteLine(num);
+}
+
+
+
+
+
+
